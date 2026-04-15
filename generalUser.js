bb9a027ea46d7c9dea1274a040c0f164d9c9ca75
barr4252 <barra@my.erau.edu>
2026-04-15 14:10:43 -0400

    trying to fix the log in with admin, member, and board member

--- generalUser.js
+++ generalUser.js
@@ -1,25 +1,2 @@
-public void checkOut(int userID, String itemName) {
-    let permit = checkPermission();
-    if (permit) {
-        int itemOut = 1; // Currently set to check out 1 item at a time, modify to check out multiple (check for + and <= available)
-        newQuantity = item.getQuantity() - itemOut;
-        item.updateQuantity(newQuantity);
 
-        recordCheckOut();
-    }
-    else {
-    // Print message saying you do not have admin permission
-    }
-}
 
-public void checkPermission(int userID, String itemName) {
-    return true; // temporarily gives everyone permission
-}
-
-public void returnItem(int userID, String itemName) {
-    // for minimum viable product, it would likely be easier to make it so that you must return all items of a specific type at once
-}
-
-public void recordCheckOut(int userID, String itemName, int itemOut) {
-    // record and store student ID, item name, and quantity of item checked out
-}  
