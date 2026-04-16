84223b21bcc42a9c1e12a802389a82b5a9d257d1
solgonzo <13solgonzo@gmail.com>
2026-04-16 16:22:01 -0400

    I tried to update/improve some of the code. I was having issues with the testing. The data format came form the events code, so it should be correctly formatted, but not integrated properly yet.

--- generalUser.js
+++ generalUser.js
@@ -1,33 +1,55 @@
+let inventoryData = [ //array to hold inventory objects
+  {
+    itemName: "Table",
+    qty: 5,
+    availableQty: 3,
+  },
 
-//general users/members 
+  {
+    itemName: "Chair",
+    qty: 4,
+    availableQty: 4,
+  }
+];
 
-public void checkOut(int userID, String itemName) {
+function checkOut(studentID, itemName) {
     let permit = checkPermission();
     if (permit) {
-        int itemOut = 1; // Currently set to check out 1 item at a time, modify to check out multiple (check for + and <= available)
-        newQuantity = item.getQuantity() - itemOut;
-        item.updateQuantity(newQuantity);
-
-
+        requestOut();
+        newQuantity = availableQty - itemOut;
+        
         recordCheckOut();
     }
     else {
-    // Print message saying you do not have admin permission
+        // Print message saying you do not have admin permission
+        showStatus("Admin permission is needed to checkout this item.");
     }
 }
 
+function requestOut(availableQty){
+    itemOut = prompt("How many would you like to check out?");
+    //itemOut = 1; // Currently set to check out 1 item at a time, modify to check out multiple (check for +, whole number, and <= available)
+    if (itemOut < 0) {
+        showStatus("Value must be positive.");
+    } else if (itemOut > availableQty) {
+        showStatus("Value cannot be more than what is available.");
+    } else if (!itemOut.isInteger) {
+        showStatus("Must be whole number.");
+    }
+    else {
+        return itemOut;
+    }
+}
 
-public void checkPermission(int userID, String itemName) {
+function checkPermission(studentID, itemName) {
     return true; // temporarily gives everyone permission
 }
 
-
-public void returnItem(int userID, String itemName) {
+function returnItem(studentID, itemName) {
     // for minimum viable product, it would likely be easier to make it so that you must return all items of a specific type at once
 }
 
-
-public void recordCheckOut(int userID, String itemName, int itemOut) {
+function recordCheckOut(studentID, itemName, itemOut) {
     // record and store student ID, item name, and quantity of item checked out
+    showStatus("The item was successfully checked out.");
 }  
-
